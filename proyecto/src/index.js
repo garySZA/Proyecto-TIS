51bc36cbd818fb9b8c4a796b2743986c4889132e
garySZA <garylvlsoliz@gmail.com>
2021-03-24 22:14:49 -0400

    Creacion del componente Barra de Navegacion

--- proyecto/src/index.js
+++ proyecto/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Barra_Sup from './Componentes/Barra_Superior/barra_superior';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Barra_Sup />
   </React.StrictMode>,
   document.getElementById('root')
 );
