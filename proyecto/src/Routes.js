01825683764619282c0a63e572a0e2e7501a58f8
XDMysterion1 <XDMysterion@gmail.com>
2021-06-03 23:08:30 -0400

    Ruta Nuevo Usario

--- proyecto/src/Routes.js
+++ proyecto/src/Routes.js
@@ -10,6 +10,7 @@ import Jefe          from './Componentes/jefe/Jefe';
 import Secretaria    from './Componentes/secretaria/Secretaria';
 import Usuario       from './Componentes/usuario/Usuario';
 import Home          from './Componentes/Home/Home';
+import NewUser       from './Componentes/Registro_de_Nuevos_Usuarios/new_user_registration'
 
 const Routes = () =>{
     return(
@@ -22,7 +23,7 @@ const Routes = () =>{
                 <Route path="/Jefe"          exact component={Jefe}/>
                 <Route path="/Secretaria"    exact component={Secretaria}/>
                 <Route path="/Usuario"       exact component={Usuario}/>
-
+                <Route path="/NewUser"       exact component={NewUser}/>
             </Switch>
         </BrowserRouter>
     )
