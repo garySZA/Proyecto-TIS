c25e76a2407b54729e31c7d9ccf00834bee2697b
garySZA <garylvlsoliz@gmail.com>
2021-06-04 00:13:44 -0400

    Implementación de funcionalidad para mostrar solicitudes dinamicamente y con restricciones

--- proyecto/src/Componentes/Mostrar_Tarjetas_Solicitudes/Mostrar_Tarjetas_Solicitudes.js
+++ proyecto/src/Componentes/Mostrar_Tarjetas_Solicitudes/Mostrar_Tarjetas_Solicitudes.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import './estilos_mostrar_tarjetas_solicitudes.css';
+import './estilos_mostrar_tarjetas-solicitudes.css';
 import Tarjeta_Solicitudes from "../Tarjeta-Solicitudes/Tarjeta-Solicitudes";
 
-function mostrar_tarjetas_solicitudes(props){
+function Mostrar_tarjetas_solicitudes(props){
+
+    let lista = ['nombre1', 'nombre2','nombre3','nombre4','nombre5','nombre6','nombre7','nombre8']
+
     return(
-        <div class="contenedor-flex-solicitudes">
-        <div class="contenedor-grid">
+        <div className="contenedor-flex-solicitudes">
+            <div className="contenedor-grid">
+                {
+                    lista.map(item =>
+                        
+                            
+                                (item != 'nombre2')?
+                                <Tarjeta_Solicitudes lista = {item}/>
+                                :
+                                ''
+                        
+                        )
+                }
+            </div>
         </div>
-    </div>
     );
 }
 
-export default mostrar_tarjetas_solicitudes;
+export default Mostrar_tarjetas_solicitudes;
