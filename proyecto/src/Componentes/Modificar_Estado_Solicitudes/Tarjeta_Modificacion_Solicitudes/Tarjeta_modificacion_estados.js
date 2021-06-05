053a7437aabc207dc9cc63a39fae72bce0310298
garySZA <garylvlsoliz@gmail.com>
2021-06-05 18:28:48 -0400

    implementación de funcionalidad para cambiar estado de solicitudes

--- proyecto/src/Componentes/Modificar_Estado_Solicitudes/Tarjeta_Modificacion_Solicitudes/Tarjeta_modificacion_estados.js
+++ proyecto/src/Componentes/Modificar_Estado_Solicitudes/Tarjeta_Modificacion_Solicitudes/Tarjeta_modificacion_estados.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import './estilos_tarjeta_modificacion_estados.css'
 
 function Tarjeta_Modificacion_Estados(props){
+    let idBotones = 'id' + props.solicitud.idFormularioSolitud
+
     let estadoRechazado= 'Rechazado'
     
-    let estados = ['En Verificacion','Esperando Autorizacion','Realizando Cotización','En Espera','Esperando Autorizacion Final','Rechazado','Aprobado']
+    let estados = ['En Verificacion','Esperando Autorizacion','Realizando Cotización','En Espera','Esperando Autorizacion Final','Aprobado']
 
     const [estado, setEstado] = useState(props.solicitud.estadoSolicitud)
 
@@ -13,10 +15,27 @@ function Tarjeta_Modificacion_Estados(props){
         document.getElementById("contenedor-botones-cambio-estado").style.display = 'none'
     }
 
+    const aprobarSolicitud = () => {
+        if(estado == 'En espera'){
+            setEstado('Esperando Autorización Final')
+            document.getElementById(props.solicitud.idFormularioSolitud).style.fontSize = "16px"
+        }else if(estado == 'En Verificación'){
+            setEstado('Esperando Autorización')
+        }else if(estado == 'Esperando Autorización'){
+            setEstado('Realizando Contización')
+        }else if(estado == 'Realizando Contización'){
+            setEstado('En Espera')
+        }else if(estado == 'Esperando Autorización Final'){
+            setEstado('Aprobado')
+            document.getElementById(idBotones).style.display = 'none'
+            document.getElementById(props.solicitud.idFormularioSolitud).style.fontSize = "22px"
+        }
+    }
+
     return(
         <div className="tarjeta-solicitud">
             <div className="contenedor-estado-solicitud">
-                <label className="estado-solicitud">{estado}</label>
+                <label className="estado-solicitud" id={props.solicitud.idFormularioSolitud}>{estado}</label>
             </div>
             <div className="contenedor-campos-solicitud">
                 <label className = "subtitulos-solicitud">Detalle:</label>
@@ -36,9 +55,9 @@ function Tarjeta_Modificacion_Estados(props){
                     <label className="etiqueta-contenido-solicitud">{props.solicitud.montoSolicitud}Bs.</label>
                 </div>
             </div>
-            <div className="contenedor-botones-cambio-estado" id="contenedor-botones-cambio-estado">
+            <div className="contenedor-botones-cambio-estado" id={idBotones}>
                 <button className="Boton-estado-solicitud boton-rechazar-solicitud" onClick={rechazarSolicitud}>Rechazar</button>
-                <button className="Boton-estado-solicitud boton-aprobar-solicitud">Aprobar</button>
+                <button className="Boton-estado-solicitud boton-aprobar-solicitud" onClick={aprobarSolicitud}>Aprobar</button>
             </div>
         </div>
     )
