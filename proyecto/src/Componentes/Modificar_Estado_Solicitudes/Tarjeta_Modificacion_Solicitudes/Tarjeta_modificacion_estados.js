c44a07030d9e0cd0e917c534e086bce68de7ff96
garySZA <garylvlsoliz@gmail.com>
2021-06-05 17:36:07 -0400

    implementacion de funcionalidad para botón Rechazar

--- proyecto/src/Componentes/Modificar_Estado_Solicitudes/Tarjeta_Modificacion_Solicitudes/Tarjeta_modificacion_estados.js
+++ proyecto/src/Componentes/Modificar_Estado_Solicitudes/Tarjeta_Modificacion_Solicitudes/Tarjeta_modificacion_estados.js
@@ -1,43 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './estilos_tarjeta_modificacion_estados.css'
 
-class Tarjeta_Modificacion_Estados extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {}
+function Tarjeta_Modificacion_Estados(props){
+    let estadoRechazado= 'Rechazado'
+    
+    let estados = ['En Verificacion','Esperando Autorizacion','Realizando Cotización','En Espera','Esperando Autorizacion Final','Rechazado','Aprobado']
+
+    const [estado, setEstado] = useState(props.solicitud.estadoSolicitud)
+
+    const rechazarSolicitud = () => {
+        setEstado(estadoRechazado)
+        document.getElementById("contenedor-botones-cambio-estado").style.display = 'none'
     }
 
-    render(){
-        return(
-            <div className="tarjeta-solicitud">
-                <div className="contenedor-estado-solicitud">
-                    <label className="estado-solicitud">{this.props.solicitud.estadoSolicitud}</label>
+    return(
+        <div className="tarjeta-solicitud">
+            <div className="contenedor-estado-solicitud">
+                <label className="estado-solicitud">{estado}</label>
+            </div>
+            <div className="contenedor-campos-solicitud">
+                <label className = "subtitulos-solicitud">Detalle:</label>
+                <div className="contenido-de-solicitudes contenedor-overflow">
+                    <label className="etiqueta-contenido-solicitud">{props.solicitud.DetalleSolitud}</label>
                 </div>
-                <div className="contenedor-campos-solicitud">
-                    <label className = "subtitulos-solicitud">Detalle:</label>
-                    <div className="contenido-de-solicitudes contenedor-overflow">
-                        <label className="etiqueta-contenido-solicitud">{this.props.solicitud.DetalleSolitud}</label>
-                    </div>
-                    <label className = "subtitulos-solicitud">Fecha de Solicitud:</label>
-                    <div className="contenido-de-solicitudes">
-                        <label className="etiqueta-contenido-solicitud">{this.props.solicitud.FechaDeSolicitud}</label>
-                    </div>
-                    <label className = "subtitulos-solicitud">Responsable:</label>
-                    <div className="contenido-de-solicitudes">
-                        <label className="etiqueta-contenido-solicitud">{this.props.solicitud.responsableSolicitud}</label>
-                    </div>
-                    <label className = "subtitulos-solicitud">Monto:</label>
-                    <div className="contenido-de-solicitudes">
-                        <label className="etiqueta-contenido-solicitud">{this.props.solicitud.montoSolicitud}</label>
-                    </div>
+                <label className = "subtitulos-solicitud">Fecha de Solicitud:</label>
+                <div className="contenido-de-solicitudes">
+                    <label className="etiqueta-contenido-solicitud">{props.solicitud.FechaDeSolicitud}</label>
                 </div>
-                <div className="contenedor-botones-cambio-estado">
-                    <button className="Boton-estado-solicitud boton-rechazar-solicitud">Rechazar</button>
-                    <button className="Boton-estado-solicitud boton-aprobar-solicitud">Aprobar</button>
+                <label className = "subtitulos-solicitud">Responsable:</label>
+                <div className="contenido-de-solicitudes">
+                    <label className="etiqueta-contenido-solicitud">{props.solicitud.responsableSolicitud}</label>
+                </div>
+                <label className = "subtitulos-solicitud">Monto:</label>
+                <div className="contenido-de-solicitudes">
+                    <label className="etiqueta-contenido-solicitud">{props.solicitud.montoSolicitud}Bs.</label>
                 </div>
             </div>
-        )
-    }
+            <div className="contenedor-botones-cambio-estado" id="contenedor-botones-cambio-estado">
+                <button className="Boton-estado-solicitud boton-rechazar-solicitud" onClick={rechazarSolicitud}>Rechazar</button>
+                <button className="Boton-estado-solicitud boton-aprobar-solicitud">Aprobar</button>
+            </div>
+        </div>
+    )
 }
 
 export default Tarjeta_Modificacion_Estados;
