e0a9d47353ce7185593d7863ec6732220d46aa0d
XDMysterion1 <XDMysterion@gmail.com>
2021-06-23 10:09:35 -0400

    ..

--- proyecto/src/Componentes/usuario/Usuario.js
+++ proyecto/src/Componentes/usuario/Usuario.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import  { useState, useEffect} from 'react';
+import './Usuario.css';
+
+import logo                                 from './img/UMSS_logo.png';
+import { Menubar }                          from 'primereact/menubar';
+import { menu }                             from './tools';
+import { Button }                           from 'primereact/button';
+import { Avatar }                           from 'primereact/avatar';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { Sidebar } from 'primereact/sidebar';
+
 
 const Usuario = () =>{
+    const [visibleRight, setVisibleRight] = useState(false);
+
+    const start = ()=>{
+        return( 
+            <div>
+                <img alt="logo" src={logo} height="60" className="p-mr-2"></img>;
+            </div>       
+
+        )
+    };
+    const [user,setUser] = useState("John");
+
+    const handleAvatar=()=>{
+        setVisibleRight(true);
+    };
+
+
+
+    const avatar =()=>{
+        return(
+            <div>
+                <Avatar icon="pi pi-user" className="p-mr-2 " size="xlarge" shape="circle" onClick={handleAvatar} style={{ backgroundColor: '#3a5795', color: '#ffffff' }}/>
+            </div>
+        )
+    };
+
     return(
-        <h4>Usuario</h4>
+        <div>
+             <div className="p-grid p-justify-center ">
+                <div className="p-col-12 rowPanel">
+                    <Menubar className="panelMenu"  start={start} model={menu} end={avatar}/>
+                </div>
+                {
+                    <Sidebar visible={visibleRight} position="right" onHide={() => setVisibleRight(false)}>
+                        <h3>Right Sidebar</h3>
+                    </Sidebar>
+                }
+            </div> 
+        </div>
     )
 }
 export default Usuario;
